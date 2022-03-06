4afcc4d1177a4c4ceb77addd7985aec816b696a3
YavuzBozkurt <yavuzbozkurt2002@gmail.com>
2022-03-06 10:58:50 +0100

    registration now works

--- app/src/main/java/com/example/ourwardrobe/Register.java
+++ app/src/main/java/com/example/ourwardrobe/Register.java
@@ -1,15 +1,27 @@
 package com.example.ourwardrobe;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.material.button.MaterialButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Register extends AppCompatActivity {
 
@@ -19,11 +31,14 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         SignButton = (Button) findViewById(R.id.signu);
         SignButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               Login();
+                Login();
             }
         });
 
@@ -37,8 +52,67 @@ public class Register extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
+                JSONObject request = new JSONObject();
 
-                Toast.makeText(Register.this, "Successfully Registered" ,Toast.LENGTH_SHORT).show();
+                try {
+                    request.put("nickname", username.getText().toString());
+                    request.put("email", Email.getText().toString());
+                    request.put("password", Password.getText().toString());
+                    request.put("gender", "M");
+                    request.put("avatar", "");
+                    request.put("OAuthToken", "M");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                URL url = null;
+
+                try {
+                    if (!Password.getText().toString().equals(RePassword.getText().toString())) {
+                        Toast.makeText(Register.this, "Passwords don't match..." ,Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    url = new URL("http://192.168.1.49:3000/users/register");
+
+                    try {
+                        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                        connection.setRequestProperty("User-Agent", "Chrome");
+                        connection.setRequestProperty("Content-Type", "application/json");
+                        connection.setDoOutput(true);
+                        connection.setRequestMethod("POST");
+                        connection.setConnectTimeout(15000);
+                        connection.setReadTimeout(15000);
+//                        connection.connect();
+
+                        DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+                        wr.writeBytes(request.toString());
+                        wr.flush();
+                        wr.close();
+
+                        int responseCode = connection.getResponseCode();
+
+                        //Response code of 200 means OK
+                        if (responseCode == 200) {
+                            Toast.makeText(Register.this, "Successfully Registered" ,Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        else{
+                            Toast.makeText(Register.this, "Could not register, something wrong with the server: " + responseCode ,Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+//                        System.out.println("Response Body : ");
+//                        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//                        String inputLine;
+//                        StringBuffer response = new StringBuffer();
+                    } catch (IOException e) {
+                        Toast.makeText(Register.this, e.getMessage() ,Toast.LENGTH_SHORT).show();
+                    }
+                } catch (MalformedURLException e) {
+                    Toast.makeText(Register.this, e.getMessage() ,Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
