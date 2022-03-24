b1deb75869d1c3a53c54e0e8a9241526601015ec
Dorukante <72575981+Dorukante@users.noreply.github.com>
2022-03-24 11:34:58 +0100

    Update Camera.java

--- app/src/main/java/com/example/ourwardrobe/Camera.java
+++ app/src/main/java/com/example/ourwardrobe/Camera.java
@@ -16,8 +16,12 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Camera extends AppCompatActivity {
 
@@ -27,6 +31,29 @@ public class Camera extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        Spinner spinner = (Spinner) findViewById(R.id.Outfits);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.Outfits, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+
+            }
+        });
 
         img = findViewById(R.id.image_view);
         bt = findViewById(R.id.open_Camera);
