dfb0f7c05edea3858de1e08ba439d584f3f606e4
NickSot <nikola.s.sotirov@gmail.com>
2022-03-27 19:40:20 +0300

    register fixed and deletion complete

--- app/src/main/java/com/example/ourwardrobe/Register.java
+++ app/src/main/java/com/example/ourwardrobe/Register.java
@@ -3,6 +3,7 @@ package com.example.ourwardrobe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,120 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Register extends AppCompatActivity {
+
+    private class RegisterRequest extends AsyncTask<Void, Void, Void> {
+        private String Password;
+        private String Nickname;
+        private String Email;
+        private String RePassword;
+
+        private int responseCode;
+        private int responseCodeWardrobe;
+
+        public RegisterRequest(String nickname,  String email, String password, String rePassword) {
+            Password = password;
+            Nickname = nickname;
+            Email = email;
+            RePassword = rePassword;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            JSONObject request = new JSONObject();
+
+            try {
+                request.put("nickname", Nickname);
+                request.put("email", Email);
+                request.put("password", Password);
+                request.put("gender", "M");
+                request.put("avatar", "");
+                request.put("OAuthToken", "M");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            JSONObject wardrobeRequest = new JSONObject();
+
+            try {
+                wardrobeRequest.put("uNickname", Nickname);
+                wardrobeRequest.put("uPassword", Password);
+                wardrobeRequest.put("nickname", "First Wardrobe");
+                wardrobeRequest.put("creationTime", null);
+                wardrobeRequest.put("wardrobeType", "Personal");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            URL url = null;
+            URL wardrobeUrl = null;
+
+            try {
+                if (!Password.equals(RePassword)) {
+                    responseCode = -1;
+                    return null;
+                }
+
+                url = new URL("http://192.168.56.1:3000/users/register");
+                wardrobeUrl = new URL("http://192.168.56.1:3000/wardrobes/register");
+
+                try {
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                    connection.setRequestProperty("Accept", "*");
+                    connection.setRequestProperty("User-Agent", "Chrome");
+                    connection.setRequestProperty("Content-Type", "application/json");
+                    connection.setDoOutput(true);
+                    connection.setRequestMethod("POST");
+                    connection.setConnectTimeout(15000);
+                    connection.setReadTimeout(15000);
+
+                    DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+                    wr.writeBytes(request.toString());
+                    wr.flush();
+                    wr.close();
+
+                    responseCode = connection.getResponseCode();
+
+                    connection = (HttpURLConnection) wardrobeUrl.openConnection();
+
+                    connection.setRequestProperty("Accept", "*");
+                    connection.setRequestProperty("User-Agent", "Chrome");
+                    connection.setRequestProperty("Content-Type", "application/json");
+                    connection.setDoOutput(true);
+                    connection.setRequestMethod("POST");
+                    connection.setConnectTimeout(15000);
+                    connection.setReadTimeout(15000);
+
+                    wr = new DataOutputStream(connection.getOutputStream());
+                    wr.writeBytes(wardrobeRequest.toString());
+                    wr.flush();
+                    wr.close();
+
+                    responseCodeWardrobe = connection.getResponseCode();
+
+                } catch (IOException e) {
+//                    Toast.makeText(Register.this, e.getMessage() ,Toast.LENGTH_SHORT).show();
+                }
+            } catch (MalformedURLException e) {
+//                Toast.makeText(Register.this, e.getMessage() ,Toast.LENGTH_SHORT).show();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void _) {
+            if (responseCode == 201 && responseCodeWardrobe == 201) {
+                Toast.makeText(Register.this, "Successfully Registered" ,Toast.LENGTH_SHORT).show();
+            }
+            else if (responseCode == -1){
+                Toast.makeText(Register.this, "Passwords don't match..." ,Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(Register.this, "Could not register, something wrong with the server: " + responseCode ,Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     private Button SignButton;
     @Override
@@ -45,100 +160,12 @@ public class Register extends AppCompatActivity {
         Register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                JSONObject request = new JSONObject();
+                RegisterRequest request = new RegisterRequest(username.getText().toString(),
+                        Email.getText().toString(),
+                        Password.getText().toString(),
+                        RePassword.getText().toString());
 
-                try {
-                    request.put("nickname", username.getText().toString());
-                    request.put("email", Email.getText().toString());
-                    request.put("password", Password.getText().toString());
-                    request.put("gender", "M");
-                    request.put("avatar", "");
-                    request.put("OAuthToken", "M");
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-//                    "uNickname": "somebullfuck",
-//                    "uPassword": "Il0veGirls",
-//                    "nickname": "smth1",
-//                    "creationTime": null,
-//                    "wardrobeType": "Personal"
-
-                JSONObject wardrobeRequest = new JSONObject();
-
-                try {
-                    wardrobeRequest.put("uNickname", username.getText().toString());
-                    wardrobeRequest.put("uPassword", Password.getText().toString());
-                    wardrobeRequest.put("nickname", "First Wardrobe");
-                    wardrobeRequest.put("creationTime", null);
-                    wardrobeRequest.put("wardrobeType", "Personal");
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                URL url = null;
-                URL wardrobeUrl = null;
-
-                try {
-                    if (!Password.getText().toString().equals(RePassword.getText().toString())) {
-                        Toast.makeText(Register.this, "Passwords don't match..." ,Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-
-                    url = new URL("http://192.168.56.1:3000/users/register");
-                    wardrobeUrl = new URL("http://192.168.56.1:3000/wardrobes/register");
-
-                    try {
-                        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-                        connection.setRequestProperty("Accept", "*");
-                        connection.setRequestProperty("User-Agent", "Chrome");
-                        connection.setRequestProperty("Content-Type", "application/json");
-                        connection.setDoOutput(true);
-                        connection.setRequestMethod("POST");
-                        connection.setConnectTimeout(15000);
-                        connection.setReadTimeout(15000);
-
-                        DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
-                        wr.writeBytes(request.toString());
-                        wr.flush();
-                        wr.close();
-
-                        int responseCode = connection.getResponseCode();
-
-                        connection = (HttpURLConnection) wardrobeUrl.openConnection();
-
-                        connection.setRequestProperty("Accept", "*");
-                        connection.setRequestProperty("User-Agent", "Chrome");
-                        connection.setRequestProperty("Content-Type", "application/json");
-                        connection.setDoOutput(true);
-                        connection.setRequestMethod("POST");
-                        connection.setConnectTimeout(15000);
-                        connection.setReadTimeout(15000);
-
-                        wr = new DataOutputStream(connection.getOutputStream());
-                        wr.writeBytes(wardrobeRequest.toString());
-                        wr.flush();
-                        wr.close();
-
-                        int responseCodeWardrobe = connection.getResponseCode();
-
-                        //Response code of 201 means CREATED
-                        if (responseCode == 201 && responseCodeWardrobe == 201) {
-                            Toast.makeText(Register.this, "Successfully Registered" ,Toast.LENGTH_SHORT).show();
-                            return;
-                        }
-                        else{
-                            Toast.makeText(Register.this, "Could not register, something wrong with the server: " + responseCode ,Toast.LENGTH_SHORT).show();
-                            return;
-                        }
-
-                    } catch (IOException e) {
-                        Toast.makeText(Register.this, e.getMessage() ,Toast.LENGTH_SHORT).show();
-                    }
-                } catch (MalformedURLException e) {
-                    Toast.makeText(Register.this, e.getMessage() ,Toast.LENGTH_SHORT).show();
-                }
+                request.execute();
             }
         });
     }
--- app/src/main/java/com/example/ourwardrobe/accountSettings.java
+++ app/src/main/java/com/example/ourwardrobe/accountSettings.java
@@ -1,20 +1,103 @@
 package com.example.ourwardrobe;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import outwardrobemodels.User;
+
 public class accountSettings extends AppCompatActivity {
 
     private Button deleteBtn, changePassword;
     private ImageView backBtn;
+
+    private class DeleteAccountRequest extends AsyncTask<Void, Void, Void> {
+        int responseCode;
+        String responseMessage;
+
+        private String Email;
+        private String Password;
+
+        DeleteAccountRequest(String Email, String Password) {
+            this.Email = Email;
+            this.Password = Password;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            JSONObject request = new JSONObject();
+
+            try {
+                request.put("uNickname", Email);
+                request.put("uPassword", Password);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            URL url = null;
+
+            try {
+                url = new URL("http://192.168.56.1:3000/users/delUser");
+
+                try {
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestProperty("User-Agent", "Chrome");
+                    connection.setRequestProperty("Content-type", "application/json");
+                    connection.setRequestMethod("DELETE");
+                    connection.setDoOutput(true);
+                    connection.setConnectTimeout(15000);
+                    connection.setReadTimeout(15000);
+
+                    DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+                    wr.writeBytes(request.toString());
+                    wr.flush();
+                    wr.close();
+
+                    Log.println(Log.DEBUG, "req", request.toString());
+
+                    responseCode = connection.getResponseCode();
+                    responseMessage = new BufferedReader(new InputStreamReader(connection.getInputStream())).readLine();
+
+                } catch (IOException e) {
+//                    Toast.makeText(loginscreen.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+            } catch (MalformedURLException e) {
+//                Toast.makeText(loginscreen.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+            return null;
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        protected void onPostExecute(Void param) {
+            if (responseCode == 200) {
+                Toast.makeText(accountSettings.this, "Successfully deleted account", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,7 +137,11 @@ public class accountSettings extends AppCompatActivity {
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(accountSettings.this, "Account Deleted", Toast.LENGTH_SHORT).show();
+                        User user = ApplicationContext.getInstance().getUser();
+                        DeleteAccountRequest request = new DeleteAccountRequest(user.getNickname(), user.getPassword());
+                        request.execute();
+                        Intent intent = new Intent(accountSettings.this, loginscreen.class);
+                        startActivity(intent);
                     }
                 })
                 .setNegativeButton("No", null);
